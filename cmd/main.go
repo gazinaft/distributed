7dfbd5622776eecf2dcd4d595a29fa9f8d644799
gazinaft <gazinaft@gmail.com>
2024-12-23 12:47:04 +0200

    init

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"html/template"
+	"io"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+func main() {
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
---
+++ sample.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"image"
+	"os"
+)
+
+func getImageFromFilePath(filepath string) (image.Image, error) {
+	f, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	image, _, err := image.Decode(f)
+	return image, err
+}
+
+func applyKernel(image.Image, float32[])
+
+func main() {
+
+}
