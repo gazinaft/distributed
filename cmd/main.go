f6416f729d8c7831d4d3d7ac27671ac2f20bba55
gazinaft <gazinaft@gmail.com>
2024-12-26 13:10:37 +0200

    basic form submission

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,13 @@
-package cmd
+package main
 
 import (
+	"fmt"
 	"html/template"
 	"io"
+	"os"
+	"path/filepath"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -22,12 +26,60 @@ func newTemplate() *Templates {
 	}
 }
 
+func SendImageToService(c echo.Context) error {
+	file, err := c.FormFile("image")
+
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Successfully submitted form")
+
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Successfully opened file")
+
+	defer src.Close()
+
+	uuid, err := uuid.NewRandom()
+
+	if err != nil {
+		return err
+	}
+
+	// get unique filename
+	filename := uuid.String() + filepath.Ext(file.Filename)
+	fmt.Printf("created uuid %s", filename)
+
+	// Destination
+	fullFilePath := fmt.Sprintf("./image/%s", filename)
+	dst, err := os.Create(fullFilePath)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
 
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Static("/images", "images")
 	e.Static("/css", "css")
+
+	e.Renderer = newTemplate()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", nil)
+	})
+
+	e.POST("/upload", SendImageToService)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
